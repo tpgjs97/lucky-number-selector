4d380383a79c92d0c350f24842d1a8f843afb39f
Thread <ghdtpgjs99@gmail.com>
2023-05-01 15:30:17 +0900

    fix: Replace i to j, Adjust

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,10 @@
-print('바보')
+for j in range(1, 16+1) :
+    if j%15 == 0 :
+        print('fizzbuzz')
+    elif j%3 == 0 :
+        print('fizz')
+    elif j%5 == 0 :
+        print('buzz')
+    else :
+        print(f'{j}')
+        

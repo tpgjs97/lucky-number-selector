3f9d5c37386dd18f09df646c9ae77801595c548e
Thread <ghdtpgjs99@gmail.com>
2023-05-01 15:31:28 +0900

    tddd

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,9 +2,4 @@ for j in range(1, 16+1) :
     if j%15 == 0 :
         print('fizzbuzz')
     elif j%3 == 0 :
-        print('fizz')
-    elif j%5 == 0 :
-        print('buzz')
-    else :
-        print(f'{j}')
-        
+        pr        
